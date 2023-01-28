d6689a7d85b3261b7d597cebda2ca25ec55c1218
Juan Luis Orozco <juanluis.ow@gmail.com>
2023-01-28 01:46:37 -0600

    HoverUpdate

--- src/componets/WeekSpends.jsx
+++ src/componets/WeekSpends.jsx
@@ -6,14 +6,14 @@ const WeekSpends = () => {
 
         <div className='w-[40px] text-center'>
             <div className='bg-soft-red h-10 group barProgress'>
-                <h4 className='barAmount'>$12.36</h4>
+                <h4 className='barAmount'>$14.20</h4>
             </div>
             <h6 className='text-medium-brown text-[12px]'>mon</h6>
         </div>
 
         <div className='w-[40px] text-center'>
             <div className='bg-soft-red h-20 group barProgress'>
-                <h4 className='barAmount'>$32.36</h4>
+                <h4 className='barAmount'>$32.84</h4>
             </div>
             <h6 className='text-medium-brown text-[12px]'>tue</h6>
         </div>
@@ -27,28 +27,28 @@ const WeekSpends = () => {
 
         <div className='w-[40px] text-center'>
             <div className='bg-soft-red h-20 group barProgress'>
-                <h4 className='barAmount'>$52.36</h4>
+                <h4 className='barAmount'>$32.84</h4>
             </div>
             <h6 className='text-medium-brown text-[12px]'>thu</h6>
         </div>
 
         <div className='w-[40px] text-center'>
             <div className='bg-soft-red h-12 group barProgress'>
-                <h4 className='barAmount'>$52.36</h4>
+                <h4 className='barAmount'>$17.77</h4>
             </div>
             <h6 className='text-medium-brown text-[12px]'>fri</h6>
         </div>
 
         <div className='w-[40px] text-center'>
             <div className='bg-soft-red h-24 group barProgress'>
-                <h4 className='barAmount'>$52.36</h4>
+                <h4 className='barAmount'>$42.69</h4>
             </div>
             <h6 className='text-medium-brown text-[12px]'>sat</h6>
         </div>
 
         <div className='w-[40px] text-center'>
             <div className='bg-soft-red h-14 group barProgress'>
-                <h4 className='barAmount'>$52.36</h4>
+                <h4 className='barAmount'>$19.11</h4>
             </div>
             <h6 className='text-medium-brown text-[12px]'>sun</h6>
         </div>
